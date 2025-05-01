5ee29db734bb3d855a392e1f423bd9603845bb7b
87 <dev@87flowers.com>
2025-05-01 19:04:27 +0100

    fix(tests/position): fix dfrc roundtrip tests

--- tests/position.cpp
+++ tests/position.cpp
@@ -4,17 +4,29 @@
 #include <tuple>
 #include <vector>
 
+#include "rose/config.h"
 #include "rose/position.h"
 #include "rose/util/assert.h"
 
 using namespace rose;
 
-auto roundtrip() -> void {
+auto roundtripClassical() -> void {
   const std::vector<std::string_view> cases{{
       "7r/3r1p1p/6p1/1p6/2B5/5PP1/1Q5P/1K1k4 b - - 0 38",
       "r3k2r/pp1bnpbp/1q3np1/3p4/3N1P2/1PP1Q2P/P1B3P1/RNB1K2R b KQkq - 5 15",
       "8/5p2/1kn1r1n1/1p1pP3/6K1/8/4R3/5R2 b - - 9 60",
       "r4rk1/1Bp1qppp/2np1n2/1pb1p1B1/4P1b1/P1NP1N2/1PP1QPPP/R4RK1 b - b6 1 11",
+  }};
+  config::frc = false;
+  for (std::string_view fen : cases) {
+    const Position position = Position::parse(fen).value();
+    const std::string result = std::format("{}", position);
+    rose_assert(result == fen, "{} != {}", fen, result);
+  }
+}
+
+auto roundtripDfrc() -> void {
+  const std::vector<std::string_view> cases{{
       "2r1kr2/8/8/8/8/8/8/1R2K1R1 w GBfc - 0 1",
       "rkr5/8/8/8/8/8/8/5RKR w HFca - 0 1",
       "2r3kr/8/8/8/8/8/8/2KRR3 w h - 3 2",
@@ -25,6 +37,7 @@ auto roundtrip() -> void {
       "4k3/8/8/8/8/8/8/2R1K3 w C - 0 1",
       "2r1k3/8/8/8/8/8/8/4K3 w c - 0 1",
   }};
+  config::frc = true;
   for (std::string_view fen : cases) {
     const Position position = Position::parse(fen).value();
     const std::string result = std::format("{}", position);
@@ -33,6 +46,7 @@ auto roundtrip() -> void {
 }
 
 auto main() -> int {
-  roundtrip();
+  roundtripClassical();
+  roundtripDfrc();
   return 0;
 }
