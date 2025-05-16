4df9cb7e703783d4b7dfbd82ce846365c4107ced
87 <dev@87flowers.com>
2025-05-16 13:43:53 +0100

    feat(move_picker): mvv-lva
    bench results:
    nodes: 42412481 nodes
    time:  5620 milliseconds
    nps:   7546161 nps

--- src/rose/move_picker.cpp
+++ src/rose/move_picker.cpp
@@ -1,8 +1,13 @@
 #include "rose/move_picker.h"
 
+#include <algorithm>
+#include <array>
+#include <ranges>
 #include <utility>
 
+#include "rose/common.h"
 #include "rose/search.h"
+#include "rose/util/static_vector.h"
 
 namespace rose {
 
@@ -21,6 +26,7 @@ namespace rose {
       {
         MoveGen movegen(m_search.m_game.position(), m_search.m_shared.movegen_precomp);
         movegen.generateMoves(m_moves);
+        sortMoves();
       }
       m_stage = Stage::emit_moves;
       [[fallthrough]];
@@ -33,6 +39,25 @@ namespace rose {
     default:
       return Move::none();
     }
+  }
+
+  auto MovePicker::sortMoves() -> void {
+    const std::array<Place, 64> &board = m_search.m_game.position().board().m;
+
+    m_first_quiet = std::stable_partition(m_moves.begin(), m_moves.end(), [](Move m) { return m.capture(); });
+    const std::span noisy{m_moves.begin(), m_first_quiet};
+    const isize noisy_count = noisy.size();
+
+    StaticVector<i32, max_legal_moves> noisy_scores;
+    noisy_scores.resize(noisy_count);
+    for (isize i = 0; i < noisy_count; i++) {
+      const Move m = m_moves[i];
+      const PieceType src_ptype = board[m.from().raw].ptype();
+      const PieceType dst_ptype = board[m.to().raw].ptype();
+      noisy_scores[i] = (dst_ptype.toSortValue() * 16 - src_ptype.toSortValue()) * 1024 - i;
+    }
+
+    std::ranges::sort(std::ranges::zip_view(noisy, noisy_scores), [](auto &&a, auto &&b) { return std::get<1>(a) > std::get<1>(b); });
   }
 
 } // namespace rose
