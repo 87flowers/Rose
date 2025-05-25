222c83a1fd4a1644b8a11c7b4d492ff47df129de
87 <dev@87flowers.com>
2025-05-25 19:40:18 +0100

    test(hash): use random hashes
    bench results:
    nodes: 75483906 nodes
    time:  16079 milliseconds
    nps:   4694333 nps

--- src/rose/hash.cpp
+++ src/rose/hash.cpp
@@ -3,50 +3,44 @@
 #include "rose/hash.h"
 
 #include <array>
+#include <random>
 
-#include "rose/util/bch/bch.h"
 #include "rose/util/types.h"
 
 namespace rose::hash {
-
-  namespace {
-    constexpr usize n = 10;
-    const std::vector<bch::Generator<n>> generators = bch::generateGenerators<n>(bch::generateMinPolys<n>());
-    const bch::Matrix<n> bm = bch::genBasisMatrix<n>(64, generators);
-    const bch::Matrix<n> pm = bch::genParityMatrix<n>(bm);
-    inline auto toHash(bch::MatrixRow<n> row) -> u64 { return static_cast<u64>(bch::bitReverse<64>(static_cast<u64>(row))); }
-  } // namespace
-
   const std::array<std::array<u64, 64>, 16> piece_table = [] {
+    std::mt19937_64 rand{0x8588A96CAD5E3985};
     std::array<std::array<u64, 64>, 16> piece_table{};
     usize i = 0;
-    for (usize ptype : {001, 002, 003, 005, 006, 007, 011, 012, 013, 015, 016, 017}) {
+    for (usize ptype = 0; ptype < 16; ptype++) {
       for (usize sq = 0; sq < 64; sq++) {
-        piece_table[ptype][sq] = toHash(pm[i++]);
+        piece_table[ptype][sq] = rand();
       }
     }
     return piece_table;
   }();
 
   const std::array<u64, 8> enpassant_table = [] {
+    std::mt19937_64 rand{0x5573B73B2BB36749};
     std::array<u64, 8> enpassant_table{};
     usize i = 64 * 12;
     for (usize j = 0; j < 8; j++) {
-      enpassant_table[j] = toHash(pm[i++]);
+      enpassant_table[j] = rand();
     }
     return enpassant_table;
   }();
 
   const std::array<std::array<u64, 2>, 2> castle_table = [] {
+    std::mt19937_64 rand{0x1F78089EF589DECF};
     std::array<std::array<u64, 2>, 2> castle_table{};
     usize i = 64 * 12 + 8;
-    castle_table[0][0] = toHash(pm[i++]);
-    castle_table[0][1] = toHash(pm[i++]);
-    castle_table[1][0] = toHash(pm[i++]);
-    castle_table[1][1] = toHash(pm[i++]);
+    castle_table[0][0] = rand();
+    castle_table[0][1] = rand();
+    castle_table[1][0] = rand();
+    castle_table[1][1] = rand();
     return castle_table;
   }();
 
-  const u64 move = toHash(pm[64 * 12 + 8 + 4]);
+  const u64 move = 0x51B2ABA9E76DEF82;
 
 } // namespace rose::hash
