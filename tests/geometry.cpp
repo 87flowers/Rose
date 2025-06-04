5cc3ea6f07c5ebd042a17f62d597fc7ad8513b28
87 <dev@87flowers.com>
2025-06-04 20:48:38 +0100

    fix(tests/geometry): update to use std::bit_cast
    bench results:
    nodes: 68488275 nodes
    time:  14415 milliseconds
    nps:   4750995 nps

--- tests/geometry.cpp
+++ tests/geometry.cpp
@@ -9,29 +9,29 @@ using namespace rose;
 
 auto testCompressCoords() -> void {
   {
-    Byteboard a;
-    Byteboard b;
+    std::array<u8, 64> a;
+    std::array<u8, 64> b;
     for (int i = 0; i < 64; i++) {
-      a.r[i] = i + (i & 0b111000);
-      b.r[i] = i;
+      a[i] = i + (i & 0b111000);
+      b[i] = i;
     }
 
-    const auto [c, valid] = geometry::internal::compressCoords(a.z);
+    const auto [c, valid] = geometry::internal::compressCoords(std::bit_cast<v512>(a));
 
-    rose_assert(c == b.z);
+    rose_assert(c == std::bit_cast<v512>(b));
     rose_assert(valid == ~u64{0});
   }
 
   {
-    Byteboard a;
-    Byteboard b;
+    std::array<u8, 64> a;
+    std::array<u8, 64> b;
     for (int i = 0; i < 64; i++) {
-      a.r[i] = i + (i & 0b111000) | 0x80;
-      b.r[i] = i + (i & 0b111000) | 0x08;
+      a[i] = i + (i & 0b111000) | 0x80;
+      b[i] = i + (i & 0b111000) | 0x08;
     }
 
-    const auto [_, valida] = geometry::internal::compressCoords(a.z);
-    const auto [_, validb] = geometry::internal::compressCoords(a.z);
+    const auto [_, valida] = geometry::internal::compressCoords(std::bit_cast<v512>(a));
+    const auto [_, validb] = geometry::internal::compressCoords(std::bit_cast<v512>(b));
 
     rose_assert(valida == 0);
     rose_assert(validb == 0);
@@ -46,24 +46,15 @@ auto testSuperpieceRays() -> void {
 
   rose_assert(bitboard == 0b0001000110010010011111000111110011101111011111000111110010010010);
 
-  Byteboard p;
-  Byteboard b;
-  p.z = perm;
-  b.z = bperm;
-
-  std::print("perm:\n");
-  p.dumpRaw();
-  std::print("\n");
-  std::print("bperm:\n");
-  b.dumpRaw();
-  std::print("\n");
+  const auto p = std::bit_cast<std::array<u8, 64>>(perm);
+  const auto b = std::bit_cast<std::array<u8, 64>>(bperm);
 
   for (int i = 0; i < 64; i++) {
     if ((valid >> i) & 1) {
-      rose_assert(b.r[p.r[i]] == i, "{:02x}: b[{:02x}] == {:02x}\n", i, p.r[i], b.r[p.r[i]]);
+      rose_assert(b[p[i]] == i, "{:02x}: b[{:02x}] == {:02x}\n", i, p[i], b[p[i]]);
     }
     if ((bitboard >> i) & 1) {
-      rose_assert(p.r[b.r[i]] == i, "{:02x}: p[{:02x}] == {:02x}\n", i, b.r[i], p.r[b.r[i]]);
+      rose_assert(p[b[i]] == i, "{:02x}: p[{:02x}] == {:02x}\n", i, b[i], p[b[i]]);
     }
   }
 }
