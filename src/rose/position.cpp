ca85eb22985b63c80e6177cffc72bdbae58352b0
87 <dev@87flowers.com>
2025-12-25 16:59:55 +0100

    found the bottleneck (420.3 Mnps)

--- src/rose/position.cpp
+++ src/rose/position.cpp
@@ -304,11 +304,13 @@ namespace rose {
 
     // A closest blocker is pinned if it has a valid pinning attacker.
 #if LPS_AVX512
-    const m8x64 no_pinner_mask {std::bit_cast<vm8x64>(std::bit_cast<u64x8>(attackers.to_vector()).zeros().to_vector()).to_bits()};
+    const m8x16 has_attacker_vecmask = u8x16 {_mm_set1_epi64x(attackers.raw)}.nonzeros();
+    const m8x64 pinned =
+      m8x64 {static_cast<u64>(_mm_cvtsi128_si64(has_attacker_vecmask.mask(u8x16 {_mm_set1_epi64x(potentially_pinned.andnot(enemy).raw)}).raw))};
 #else
     const m8x64 no_pinner_mask = std::bit_cast<m8x64>(std::bit_cast<m64x8>(attackers).to_vector().zeros());
-#endif
     const m8x64 pinned = potentially_pinned.andnot(enemy).andnot(no_pinner_mask);
+#endif
 
     // Translate to valid move rays
     const u8x64 pinned_ids = pin_raymasks.mask(geometry::lane_broadcast(pinned.mask(ray_places)));
