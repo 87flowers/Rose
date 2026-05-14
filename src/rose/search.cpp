e47e7c9df4ad6b9336944e70c466f566fc4a401a
87 <dev@87flowers.com>
2026-05-14 16:13:34 +0200

    Bench: 4950110

--- src/rose/search.cpp
+++ src/rose/search.cpp
@@ -413,12 +413,12 @@ namespace rose {
 
       if (!best_move.capture()) {
         m_quiet_history.update(stm, best_move, quiet_bonus);
-        for (i32 i : {1, 2})
+        for (i32 i : {1, 2, 3})
           if (ss[-i].conthist)
             ss[-i].conthist->update(stm, position.place_at(best_move.from()).ptype(), best_move, cont_bonus);
         for (const Move quiet : fail_low_quiets) {
           m_quiet_history.update(stm, quiet, -quiet_malus);
-          for (i32 i : {1, 2})
+          for (i32 i : {1, 2, 3})
             if (ss[-i].conthist)
               ss[-i].conthist->update(stm, position.place_at(quiet.from()).ptype(), quiet, -cont_malus);
         }
