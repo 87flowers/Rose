aaa79817db12e70c09eeec85586a1c0f06ca587f
87 <dev@87flowers.com>
2026-05-25 13:33:56 +0200

    Bench: 9153119

--- src/rose/search.cpp
+++ src/rose/search.cpp
@@ -338,8 +338,8 @@ namespace rose {
     }
 
     // Internal Iterative Deepening
-    if (expected == NodeType::pv && depth >= 4 && !disable_pruning && tte.bound == NodeType::none) {
-      const i32 reduced_depth = depth / 2;
+    if (expected == NodeType::pv && depth >= 4 && ss->excluded.is_none() && tte.bound == NodeType::none) {
+      const i32 reduced_depth = depth - 2;
 
       ss->disable_pruning = true;
       const Score reduced_score = search<expected>(ctrl, position, pv, alpha, beta, ss, ply, reduced_depth);
