4b8368c81c3ec7f8b11bf793023e87ba1de0725a
87 <dev@87flowers.com>
2026-06-12 18:52:38 +0200

    Bench: 5616338

--- src/rose/move_picker.cpp
+++ src/rose/move_picker.cpp
@@ -142,7 +142,7 @@ namespace rose {
       for (i32 i : conthists_indexes)
         if (m_ss[-i].conthist)
           score += m_ss[-i].conthist->get(stm, ptype, mv);
-      score += 4096 * (threats.read(mv.from()) && ptype != PieceType::p);
+      score += 1024 * (threats.read(mv.from()) && ptype != PieceType::p);
 
       scores[i] = score * 256 - i;
     }
