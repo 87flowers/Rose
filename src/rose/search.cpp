6c2bbcf4afa25e314214f43705b38642893d9d79
87 <dev@87flowers.com>
2026-08-11 13:03:14 +0200

    Bench: 6206005

--- src/rose/search.cpp
+++ src/rose/search.cpp
@@ -412,12 +412,12 @@ namespace rose {
       }
 
       // Alpha ProbCut
-      const Score probcut_alpha = alpha - 700_z;
-      if (expected == NodeType::all && depth >= 8 && hint_move.is_none() && static_eval < probcut_alpha) {
+      const Score probcut_alpha = alpha - 300_z - 500_z * depth;
+      if (expected == NodeType::all && depth >= 8 && hint_move.is_none() && static_eval < alpha) {
         const Score score = search<expected>(ctrl, position, pv, probcut_alpha, probcut_alpha + 1, ss, ply, depth - 4);
 
         if (score <= probcut_alpha)
-          return probcut_alpha;
+          return score;
       }
     }
 
