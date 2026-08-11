3ef4be82e9dd1344e8e6c4fc61af674fe4775c81
87 <dev@87flowers.com>
2026-08-11 13:51:59 +0200

    Bench: 6923818

--- src/rose/search.cpp
+++ src/rose/search.cpp
@@ -414,9 +414,9 @@ namespace rose {
       // Alpha ProbCut
       const Score probcut_alpha = alpha - 300_z - 500_z * depth;
       if (expected == NodeType::all && depth >= 8 && hint_move.is_none() && static_eval < alpha && !ss->in_alpha_probcut) {
-        ss->in_alpha_probcut = false;
-        const Score score = search<expected>(ctrl, position, pv, probcut_alpha, probcut_alpha + 1, ss, ply, depth - 4);
         ss->in_alpha_probcut = true;
+        const Score score = search<expected>(ctrl, position, pv, probcut_alpha, probcut_alpha + 1, ss, ply, depth - 4);
+        ss->in_alpha_probcut = false;
 
         if (score <= probcut_alpha)
           return score;
