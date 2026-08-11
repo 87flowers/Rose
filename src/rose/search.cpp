1b50b115aafeeeeec787dc50f985f97313c0509e
87 <dev@87flowers.com>
2026-08-11 19:02:44 +0200

    Bench: 6444634

--- src/rose/search.cpp
+++ src/rose/search.cpp
@@ -540,6 +540,7 @@ namespace rose {
         reduction -= 970_z * (expected == NodeType::pv);
         reduction -= 132_z * history / 1024;
         reduction += 937_z * (expected == NodeType::cut);
+        reduction += 512_z * (expected == NodeType::cut && hint_move.is_none());
         reduction -= 844_z * child_position.is_in_check();
 
         const i32 lmr_depth = std::min(std::max(new_depth - reduction / 1024, 0), new_depth) + (expected == NodeType::pv);
