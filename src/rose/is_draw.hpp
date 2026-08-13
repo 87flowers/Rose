5e6947ecbc89d6dd99940bda43b939d3ffd20e88
87 <dev@87flowers.com>
2026-08-13 18:01:14 +0200

    Bench: 6650691

--- src/rose/is_draw.hpp
+++ src/rose/is_draw.hpp
@@ -22,14 +22,19 @@ namespace rose::draw {
     return 0;
   }
 
-  inline auto is_repetition(const std::vector<Hash>& hash_stack, usize hash_waterline) -> bool {
+  inline auto is_repetition(const Position& pos, const std::vector<Hash>& hash_stack, usize hash_waterline) -> bool {
     const int height = static_cast<int>(hash_stack.size()) - 1;
-    const Hash current_hash = hash_stack[height];
+    const int end = std::min<int>(pos.fifty_move_clock(), height);
+
+    const auto hash_at = [&hash_stack, height](int i) {
+      return hash_stack[height - i];
+    };
+    const Hash current_hash = hash_at(0);
 
     usize clones = 0;
-    for (int i = height - 4; i >= 0; i -= 2) {
-      if (hash_stack[i] == current_hash) {
-        const usize clone_limit = i < hash_waterline ? 2 : 1;
+    for (int i = 4; i <= end; i += 2) {
+      if (hash_at(i) == current_hash) {
+        const usize clone_limit = (height - i) < hash_waterline ? 2 : 1;
         clones++;
         if (clones >= clone_limit)
           return true;
