74e73e14baed608e1808fb2714093a0146004889
87 <dev@87flowers.com>
2026-08-15 12:38:56 +0200

    Bench: 7554588

--- src/rose/search.cpp
+++ src/rose/search.cpp
@@ -501,6 +501,8 @@ namespace rose {
           extension += expected != NodeType::pv && singular_score <= singular_beta - 21_z;
           // Triple extension
           extension += expected != NodeType::pv && singular_score <= singular_beta - 131_z;
+          // Quadruple extension
+          extension += expected != NodeType::pv && ply <= depth / 4 && singular_score <= singular_beta - 270_z;
         }
         // Negative extension
         else if (expected == NodeType::cut) {
