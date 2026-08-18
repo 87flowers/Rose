b275152c4d0eab242da169178e53d9fe0216bab8
87 <dev@87flowers.com>
2026-08-18 20:40:16 +0200

    Move tunable UDL to tune.hpp
    No functional change.
    
    Bench: 7080100

--- src/rose/common.hpp
+++ src/rose/common.hpp
@@ -186,14 +186,6 @@ namespace rose {
 
   static_assert(sizeof(PieceType) == sizeof(u8));
 
-  constexpr auto operator""_z(unsigned long long value) -> i32 {
-    return static_cast<i32>(value);
-  }
-
-  constexpr auto operator""_z(long double value) -> f64 {
-    return static_cast<f64>(value);
-  }
-
 }  // namespace rose
 
 template<>
--- src/rose/tune.hpp
+++ src/rose/tune.hpp
@@ -4,6 +4,23 @@
 
 #include <string_view>
 
+namespace rose {
+
+  // clang-format off
+#define rose_define_tunable_marker(postfix)                                                                                                          \
+  constexpr auto operator""##postfix(unsigned long long value) -> i32 {                                                                              \
+    return static_cast<i32>(value);                                                                                                                  \
+  }                                                                                                                                                  \
+                                                                                                                                                     \
+  constexpr auto operator""##postfix(long double value) -> f64 {                                                                                     \
+    return static_cast<f64>(value);                                                                                                                  \
+  }
+  // clang-format on
+
+  rose_define_tunable_marker(_z);
+
+}  // namespace rose
+
 namespace rose::tune {
 
   // clang-format off
