433781fdb657f03642b050b3754ba0651ea5ecec
87 <dev@87flowers.com>
2026-08-20 20:53:23 +0200

    Bench: 6794176

--- src/rose/search.cpp
+++ src/rose/search.cpp
@@ -561,12 +561,13 @@ namespace rose {
         i32 reduction;
 
         if (mv.is_noisy()) {
-          reduction = 1721_z + 180_z * log2_depth * log2_searched_moves;
+          reduction = 1221_z + 180_z * log2_depth * log2_searched_moves;
+          reduction -= 95_z * history / 1024;
         } else {
           reduction = 2255_z + 214_z * log2_depth * log2_searched_moves;
+          reduction -= 132_z * history / 1024;
         }
         reduction -= 970_z * (expected == NodeType::pv);
-        reduction -= 132_z * history / 1024;
         reduction += 937_z * (expected == NodeType::cut);
         reduction -= 844_z * child_position.is_in_check();
 
